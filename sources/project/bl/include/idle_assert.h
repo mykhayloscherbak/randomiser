cca3ae3898f7ddf831082143f0431ff2e824d1de
Mykhaylo Shcherbak <mikl74@yahoo.com>
2020-07-14 00:53:57 +0300

    Unneeded file deleted

--- sources/project/bl/include/idle_assert.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef SOURCES_PROJECT_BL_INCLUDE_IDLE_ASSERT_H_
-#define SOURCES_PROJECT_BL_INCLUDE_IDLE_ASSERT_H_
-
-void vAssertCalled( unsigned long ulLine, const char * const pcFileName );
-
-#endif /* SOURCES_PROJECT_BL_INCLUDE_IDLE_ASSERT_H_ */
--- sources/project/hal/include/ds3231m.h
+++
@@ -1,8 +0,0 @@
-#ifndef SOURCES_PROJECT_HAL_INCLUDE_DS3231M_H_
-#define SOURCES_PROJECT_HAL_INCLUDE_DS3231M_H_
-
-void DS3231M_add_to_queue(const uint16_t ticks);
-void Create_DS3231_Task(void);
-
-
-#endif /* SOURCES_PROJECT_HAL_INCLUDE_DS3231M_H_ */
