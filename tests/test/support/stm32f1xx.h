3ca034b8cdef12427e1bc436e934deac841350ef
Mykhaylo Shcherbak <mis@ektos.net>
2021-09-23 11:38:19 +0300

    Test framework added

---
+++ tests/test/support/stm32f1xx.h
@@ -0,0 +1,11 @@
+#ifndef SUPPORT_STM32F1XX_H
+#define SUPPORT_STM32F1XX_H
+typedef void * GPIO_TypeDef;
+#define GPIOA ((void *)0x120601)
+#define GPIOB ((void *)0x120602)
+#define GPIOC ((void *)0x120603)
+#define GPIOD ((void *)0x120604)
+#define GPIOE ((void *)0x120605)
+
+
+#endif 
---
+++ tests/test/test_bll.c
@@ -0,0 +1,26 @@
+#include "unity.h"
+#include "Bll.h"
+#include "fonts.h"
+#include "arial72.h"
+#include "arial8.h"
+#include "mock_Gpio.h"
+#include "mock_pwm.h"
+#include "mock_Clock.h"
+#include "mock_power.h"
+#include "mock_uc1701x.h"
+#include "mock_buttons.h"
+#include "mock_Spi.h"
+
+
+void test_bll_call(void)
+{
+   GetTicksCounter_ExpectAndReturn(5);
+   Buttons_Process_Expect(0);
+   Gpio_Set_Bit_Ignore();
+   SetTimer_Ignore();
+   IsTimerPassed_IgnoreAndReturn(0);
+   TEST_ASSERT_EQUAL(MainLoop_Iteration(),1);
+   GetTicksCounter_ExpectAndReturn(5);
+   TEST_ASSERT_EQUAL(MainLoop_Iteration(),0);
+
+}
